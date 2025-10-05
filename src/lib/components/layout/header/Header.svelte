e1bfcb1cb7e7637a3a1c93ed0a8269868ad3a2a9
saha <acidoralkali@gmail.com>
2025-10-05 21:46:47 +0300

    test2

--- src/lib/components/layout/header/Header.svelte
+++ src/lib/components/layout/header/Header.svelte
@@ -9,7 +9,7 @@
 	import bag from '$lib/assets/icon-bag-24x24.svg';
 	import { itemCount } from '$lib/stores/cartStore.js';
 
-	const phone = '+380990000111';
+	const phone = '+380990001111';
 
 	let isMenuOpen = $state(false);
 
