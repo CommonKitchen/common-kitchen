ab00a2a3ee1b6de636c7f4ebd0f9d9f4e4d6843e
saha <acidoralkali@gmail.com>
2025-10-29 19:35:03 +0200

    debug redirect

--- src/routes/payment/callback/+server.js
+++ src/routes/payment/callback/+server.js
@@ -4,9 +4,9 @@ import { redirect } from '@sveltejs/kit';
 export async function POST({ request }) {
 	console.log('POST');
 	try {
-		const paymentData = await request.json();
+		//const paymentData = await request.json();
 
-		console.log('paymentData', paymentData);
+		//console.log('paymentData', paymentData);
 		throw redirect(303, `/payment`);
 	} catch (error) {
 		console.error('Ошибка обработки WayForPay POST:', error);
