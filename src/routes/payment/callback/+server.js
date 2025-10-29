6beef39d36331019918da97e7126db096775b26e
saha <acidoralkali@gmail.com>
2025-10-29 20:50:32 +0200

    debug redirect

--- src/routes/payment/callback/+server.js
+++ src/routes/payment/callback/+server.js
@@ -2,12 +2,30 @@
 import { redirect } from '@sveltejs/kit';
 
 export async function POST({ request }) {
-	console.log('POST', request);
-	try {
-		//const paymentData = await request.json();
+	const contentType = request.headers.get('content-type') || '';
+	let paymentData;
 
-		//console.log('paymentData', paymentData);
-		throw redirect(303, `/payment`);
+	try {
+		if (
+			contentType.includes('multipart/form-data') ||
+			contentType.includes('application/x-www-form-urlencoded')
+		) {
+			const data = await request.formData();
+			// Преобразуем FormData в обычный объект JavaScript
+			paymentData = Object.fromEntries(data.entries());
+		} else if (contentType.includes('application/json')) {
+			paymentData = await request.json();
+		} else {
+			// Неизвестный или неподдерживаемый тип контента
+			return new Response(JSON.stringify({ message: 'Unsupported Content Type' }), {
+				status: 400,
+				headers: { 'Content-Type': 'application/json' }
+			});
+		}
+
+		console.log('paymentData (From WayForPay)', paymentData);
+
+		throw redirect(303, `/payment?status=success`);
 	} catch (error) {
 		console.error('Ошибка обработки WayForPay POST:', error);
 		throw redirect(303, `/payment?status=error`);
