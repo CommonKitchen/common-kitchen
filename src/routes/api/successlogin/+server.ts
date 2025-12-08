1fbbc4b66a8dab362a3e5d3932cb0d73856a30bb
saha <acidoralkali@gmail.com>
2025-12-08 10:25:24 +0200

    new login

--- src/routes/api/successlogin/+server.ts
+++ src/routes/api/successlogin/+server.ts
@@ -5,26 +5,15 @@ import type { RequestHandler } from './$types';
 export const POST: RequestHandler = async ({ request, cookies }) => {
 	const { sessionId } = await request.json();
 
-	cookies.set('auth_session_id', sessionId, {
-		path: '/',
-		maxAge: 60 * 60 * 24 * 7,
-		httpOnly: true,
-		secure: true,
-		sameSite: 'lax'
-	});
-
-	let customerData = null;
-
 	const API_URL = `https://${API_SERVER_URL}/cakes/hs/shop/customers`;
 
 	try {
 		const res = await fetch(API_URL, {
 			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
+			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({ sessionId })
 		});
+
 		if (!res.ok) {
 			console.error(`Telegram bot API responded with ${res.status}: ${res.statusText}`);
 			return new Response(JSON.stringify({ error: `External API error: ${res.status}` }), {
@@ -32,16 +21,26 @@ export const POST: RequestHandler = async ({ request, cookies }) => {
 				headers: { 'Content-Type': 'application/json' }
 			});
 		}
-		customerData = await res.json();
-	} catch (err) {
-		console.error('Failed to fetch customer data from Telegram bot:', err);
-		customerData = null;
-	}
 
-	return new Response(JSON.stringify({ customer: customerData }), {
-		status: 200,
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
+		const customerData = await res.json();
+
+		cookies.set('auth_session_id', sessionId, {
+			path: '/',
+			maxAge: 60 * 60 * 24 * 7,
+			httpOnly: true,
+			secure: true, // Внимание: может вызвать проблемы на http://localhost
+			sameSite: 'lax'
+		});
+
+		return new Response(JSON.stringify({ customer: customerData }), {
+			status: 200,
+			headers: { 'Content-Type': 'application/json' }
+		});
+	} catch (err) {
+		console.error('Failed to fetch customer data or process request:', err);
+		return new Response(JSON.stringify({ error: 'Internal server error' }), {
+			status: 500,
+			headers: { 'Content-Type': 'application/json' }
+		});
+	}
 };
